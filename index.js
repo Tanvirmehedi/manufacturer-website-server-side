64ba5cb7caf1f1a495b4eb056cadf7e45d0a92bf
Tanvirmehedi <tjmehedi201@gmail.com>
2022-05-22 22:21:47 +0600

    mongodb database added

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 //MeddleWare ----------------------------------------------------------
 app.use(cors());
 app.use(express.json());
 //MeddleWare-----------------------------------------------------------
+
+//CONNECTION mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rlvoc.mongodb.net/?retryWrites=true&w=majority`;
+
 app.get("/", (req, res) => {
-  res.send("Welcome Toolefy  Website ");
+  res.send("Welcome Toolify  Website ");
 });
 
 app.listen(port, () => {
-  console.log(`Toolefy App listening on port ${port}`);
+  console.log(`Toolify App listening on port ${port}`);
 });
